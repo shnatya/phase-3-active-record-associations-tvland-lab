62347823214cf32966772ea2ed4bf76d8e57cab0
Anastasia Shatokhina <shnatya@gmail.com>
2022-02-11 10:20:03 -0800

    commit tree 21777761b63a045ca53ea5eac4037a0d913cadce

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        arr =[]
+        self.actors.each do |a|
+          arr <<  "#{a.first_name} #{a.last_name}"
+        end
+        arr
+    end
 end
